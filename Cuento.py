8d7cc70bfa15e803ce79ba4059a8654f4f8e30ac
A01745465 <69481106+A01745465@users.noreply.github.com>
2021-09-17 14:23:21 -0500

    Código
    Se crean con el uso de diccionarios frecuencias de cada palabra, posteriormente son metidas a una lista por metódo de for para poder graficar

--- Cuento.py
+++ Cuento.py
@@ -1,1 +1,21 @@
-open("GEH.txt","r")
+import matplotlib.pyplot as plot
+palabras=[]
+frecuencias=[]
+with open("GEH.txt") as f_obj:
+    cuento=f_obj.read()
+    cuento=cuento.lower()
+    palabras= cuento.split()
+    diccionario_frecuencias={}
+    for palabra in palabras:
+        if palabra in diccionario_frecuencias:
+            diccionario_frecuencias[palabra]+=1
+        else:
+            diccionario_frecuencias[palabra]=1
+    for palabra in diccionario_frecuencias:
+        frecuencia=diccionario_frecuencias[palabra]
+        palabras.append({palabra})
+        frecuencias.append({frecuencia})
+       
+print(palabras)
+print(frecuencias)
+        
