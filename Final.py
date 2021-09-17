4cab017af52e456bc4a16f8068cfefa09c4a1cd0
A01753240 <69489832+A01753240@users.noreply.github.com>
2021-09-17 16:45:46 -0500

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,148 +1,80 @@
-import HCRfinal
-import pygame
+import random#Paqueteria usada para dar soluciones aleatorias al acertijo 
 
+Lado_A = ['Granjero', 'Zorro', 'Ganzo', 'Maiz']
+#variable A con la lista de los nombres de los personajes
+#Las listas sirven para almacenar los datos 
+Lado_B = []
+#variable b con lista vacia 
+Path = []
+#variable path con lista vacia  
 
-def redrawGameWindow(Dir, p1, p2):
-    global x, y, Side_A, Side_B
+def seleccion(L):
+  #primera funcion de codigo que se encanga de la seleccion de un numero aleatoreo entero, con parametro L
+    op = random.randint(0,len(L)-1)#Devuelve un entero aleatorio N tal que a <= N <= b. Alias ​​de rango aleatorio (a, b + 1).
+    return (L[op]) #regresa L
+
+def Viaje(F, D):
+  #funcion viaje con los paramentros F y D que se encargara de remover algunos elementos de la lista principal 
+    p1 = seleccion(F)
+    #print ('Selección -> ', p1)
+    if p1 != 'Granjero':
+      #ciclo if para el mejor manejo de remove 
+        F.remove(p1) #busca entre los elementos para eliminar el elemento del granjero en la lista 
+        D.append(p1) #añade al final de la lista lo que se desea 
+
+    F.remove('Granjero')
+    D.append('Granjero')
+    #print (F)
+    #print (D)
+    return ('Granjero',p1) #Regresa en la variable Granjero
+
+def valida_estado(L):  #Funcion con parametro L en donde se encuenta el ciclo if 
+    if 'Maiz' in L and 'Ganzo' in L and len(L) == 2: #Ciclo if que busca un elemnto en el rango L 
+        return False #sino se cumple se vuelve a correr el ciclo 
+    elif 'Zorro' in L and 'Ganzo' in L and len(L) == 2:
+      #segunda alternativa del codigo ahora con los elemnetos de la lista restantes 
+        return False#sino se cumple se vuelve a correr el ciclo 
+    return True#si alguno se cumple se cierra ciclo 
+
+def reiniciar_sistema():
+  #funcion de reiniciar sistema 
+    global Lado_A, Lado_B, Path
+    
+    Lado_A = ['Granjero', 'Zorro', 'Ganzo', 'Maiz']
+    Lado_B = []
+    Path = []
+
+def HCR(): #
+    F = Lado_A
+    D = Lado_B
+    while len(Lado_B) != 4:
+        p1, p2 = Viaje(F, D)
+        if valida_estado (F) and valida_estado (D):
+            #print ('Estado valido, continuamos')
+            if F == Lado_A:
+                Path.append('A->B :')
+            else:
+                Path.append('B->A :')
+            Path.append(p1)
+            Path.append(p2)
             
-    win.blit(bg,(0,0))
-    ypos = 300
-    for item in Side_A:
-        win.blit(item,(5,ypos))
-        ypos = ypos - 60
-
-    ypos = 300
-    for item in Side_B:
-        win.blit(item,(450,ypos))
-        ypos = ypos - 60
-
-    if p1 != 'Unknown':
-        if right:
-            win.blit(BoatRight,(x,y))
-            win.blit(farmer,(x,y-50))
-            if p2 != farmer:
-                win.blit(p2,(x+50,y-50))           
-        elif left:
-            win.blit(BoatLeft,(x,y))
-            win.blit(farmer,(x,y-50))
-            if p2 != farmer:
-                win.blit(p2,(x+50,y-50))            
-    else:
-        win.blit(char,(x, y))
-    pygame.display.update()
-
-
-def get_characters(d, p1, p2):
-    if p2 == 'Zorro':
-        character = fox
-    elif p2 == 'Maiz':
-        character = corn
-    elif p2 == 'Ganzo':
-        character = duck
-    else:
-        character = farmer
-    return (d, farmer, character)
-
-
-def Embark_characters(B, p1, p2):
-    if p1 in B:
-        B.remove(p1)     
-    if p2 in B:
-        B.remove(p2)
-
-
-def Disembark_characters(A, p1, p2):
-    if p1 not in A:
-        A.append(p1)
-    if p2 not in A:
-        A.append(p2)
-
-
-def HCR_animacion(P):
-    global x, y, left, right, vel
-    global Side_A, Side_B
-
-    clock = pygame.time.Clock()
-    run = True
-    move = 0
-    while run:
-        clock.tick(27)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            left = True
-            right = False
-            if move < len(P):
-                direction, p1, p2 = get_characters(P[move], P[move + 1], P[move + 2])
-                Embark_characters(Side_B, p1, p2)
-                for step in range(65):
-                    x -= vel
-                    redrawGameWindow(direction, p1, p2)
-                    pygame.time.delay(70)
-                move += 3
-                Disembark_characters(Side_A, p1, p2)
-
-        elif keys[pygame.K_RIGHT]:
-            right = True
-            left = False
-            if move < len(P):
-                direction, p1, p2 = get_characters(P[move], P[move + 1], P[move + 2])
-                Embark_characters(Side_A, p1, p2)
-                for step in range(65):
-                    x += vel
-                    redrawGameWindow(direction, p1, p2)
-                    pygame.time.delay(70)
-                move += 3
-                Disembark_characters(Side_B, p1, p2)
+            Temp = F
+            F = D
+            D = Temp      
         else:
-            redrawGameWindow ('Standby','Unknown', 'Unknown')
-        
+            #print ('Estado inválido, REINICIO DEL SISTE;A')
+            reiniciar_sistema()
+            F = Lado_A
+            D = Lado_B
+    return (Path)
 
-    pygame.quit()
-
-def Busca_solucion():
-    P = HCRfinal.HCR()
+def main():
+    P = HCR()
     while len(P) > 22:
-    #while len(P) > 42:
-        HCRfinal.reiniciar_sistema()
+        reiniciar_sistema()
         print ('\nBuscando una mejor solución, Longitud del Path', len(P))
-        P = HCRfinal.HCR()
+        P = HCR()
     print (P)
     print (len(P))
-    print ('\n =====> Solución encontrada:')
-    return (P)
-
             
-P = Busca_solucion()
-print ('Aquí su animación')
-
-pygame.init()
-
-win = pygame.display.set_mode((500,500))
-pygame.display.set_caption("How to Cross the River")
-
-BoatRight   = pygame.image.load('BoteRight.png')
-BoatLeft    = pygame.image.load('BoteLeft.png')
-bg          = pygame.image.load('seaL.png')
-char        = pygame.image.load('BoteRight.png')
-fox         = pygame.image.load('fox.png')
-corn        = pygame.image.load('corn.png')
-duck        = pygame.image.load('duck.png')
-farmer      = pygame.image.load('farmer.png')
-x       = 10
-y       = 425
-vel     = 5
-left    = False
-right   = False
-
-Side_A = [farmer, fox, duck, corn]
-Side_B = []
-
-HCR_animacion(P)
-
-
-
-
+main()
