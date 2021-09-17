e432ac9ca0cfbeb414f0147a6b924351fbafc699
A01745465 <69481106+A01745465@users.noreply.github.com>
2021-09-17 15:57:34 -0500

    Plots y comentarios

--- Cuento.py
+++ Cuento.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plot
-palabras=[]
+
+
+palabrasL=[] #Creamos dos listas vacías para agregar los elementos de repeticiones y palabras
 frecuencias=[]
-with open("GEH.txt") as f_obj:
-    cuento=f_obj.read()
-    cuento=cuento.lower()
-    palabras= cuento.split()
-    diccionario_frecuencias={}
-    for palabra in palabras:
+with open("GEH.txt") as f_obj: #Abrir el txt donde esta el cuento
+    cuento=f_obj.read() #Es necesario leerlo
+    cuento=cuento.lower() #Eliniminamos todas las letras mayúsculas
+    palabras= cuento.split() #Separamos los valores
+    diccionario_frecuencias={} #Creamos un diccionario de frecuencias vacío
+    for palabra in palabras: #Este for va analizando todas las palabras
         if palabra in diccionario_frecuencias:
-            diccionario_frecuencias[palabra]+=1
+            diccionario_frecuencias[palabra]+=1 #Con este if podemos agregar 1 cad vez que encuentre una palabra repetida
         else:
-            diccionario_frecuencias[palabra]=1
-    for palabra in diccionario_frecuencias:
+            diccionario_frecuencias[palabra]=1 #Si encuentra una palabra nueva se le asigna un valor de 1
+    #print("cdt: ",diccionario_frecuencias)
+    for palabra in diccionario_frecuencias: #Con este for agregamos las palabras y las frecuencias a las listas vacías
         frecuencia=diccionario_frecuencias[palabra]
-        palabras.append({palabra})
-        frecuencias.append({frecuencia})
+        palabrasL.append(palabra)
+        frecuencias.append(frecuencia)
+    
+plot.plot(palabrasL,frecuencias) #Cuando tenemos nuestras listas graficamos.
+plot.show()
+
+
+#print (palabrasL)
+#print (frecuencias)
+
+
        
-print(palabras)
-print(frecuencias)
+
         
